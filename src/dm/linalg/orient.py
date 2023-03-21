a0bc212f756376f57716ffe7ec35bd67f1a6fb02
DangerMouseB <dangermouseb@forwarding.cc>
2023-03-21 06:55:53 +0000

    licensing under apache

--- src/dm/linalg/orient.py
+++ src/dm/linalg/orient.py
@@ -1,11 +1,18 @@
 # **********************************************************************************************************************
-#    This is a derivation of Jay Damask's work from:
+# This is a derivation of Jay Damask's work from:
 #       https://gitlab.com/thucyd-dev/thucyd, and,
 #       https://gitlab.com/thucyd-dev/thucyd-eigen-working-examples
-#    Original Copyright 2019 Buell Lane Press, LLC (buell-lane-press.co)
-#    Licensed under Apache License, Version 2.0, January 2004, see http://www.apache.org/licenses/
+# Original Copyright 2019 Buell Lane Press, LLC (buell-lane-press.co)
+# Licensed under Apache License, Version 2.0, January 2004, see http://www.apache.org/licenses/
 #
-#    Changes are Copyright (c) 2022 David Briant. All rights reserved.
+# Changes are Copyright (c) 2022 David Briant. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
 #
 # DESCRIPTION:
 #       Implementation of the theory of consistently oriented eigenvectors
