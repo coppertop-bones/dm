21faa6c89acd5aa42ad23a666d71c1d9a3a40db2
DangerMouseB <dangermouseb@forwarding.cc>
2025-05-28 19:13:16 +0100

    wip - tests

--- tests/bones/jones/test_em.py
+++ tests/bones/jones/test_em.py
@@ -1,4 +1,13 @@
-import sys, itertools
+# **********************************************************************************************************************
+# Copyright 2025 David Briant, https://github.com/coppertop-bones. Licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the License. You may obtain a copy of the  License at
+# http://www.apache.org/licenses/LICENSE-2.0. Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  KIND,
+# either express or implied. See the License for the specific language governing permissions and limitations under the
+# License. See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+# **********************************************************************************************************************
+
+import sys, itertools, pytest
 
 from coppertop.pipe import *
 
@@ -20,9 +29,9 @@ def apply_(fn, args:pylist+pytuple):
     return lambda : fn(*args)
 
 
-
-def test_em(k):
-    k = jones.Kernel() if k is Missing else k
+@pytest.mark.xfail
+def test_em():
+    k = jones.Kernel()
     em = k.em
 
     e = em.enum('excl', 'mem')
@@ -44,14 +53,12 @@ def test_em(k):
 
 
 
-def main(k=Missing):
-    test_em(k) >> PP
+def main():
+    test_em() >> PP
 
 
 
 if __name__ == '__main__':
-    sys._k = jones.Kernel()
-    main(sys._k)
-    sys._k = None
+    main()
     'passed' >> PP
 
